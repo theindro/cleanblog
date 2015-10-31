466b08e821e3966ec213ae1b9c613a5cb73e485d
Indro <indro.malleus@khk.ee>
2015-10-31 16:21:15 +0200

    parandus

--- include/database.php
+++ include/database.php
@@ -6,7 +6,9 @@
  * Time: 15:00
  */
 //connect database
-$db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
-mysqli_query($db, "SET NAMES 'utf8'");
+define('DATABASE_HOSTNAME', '127.0.0.1');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_DATABASE', 'cleanblog');
 
 require "config.sample.php";
