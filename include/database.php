3e34ce7a657a0f014f663bc77c1e2de042627e85
Indro <indro.malleus@khk.ee>
2015-10-31 16:22:04 +0200

    parandus

--- include/database.php
+++ include/database.php
@@ -6,9 +6,5 @@
  * Time: 15:00
  */
 //connect database
-define('DATABASE_HOSTNAME', '127.0.0.1');
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_DATABASE', 'cleanblog');
 
 require "config.sample.php";
