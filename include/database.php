28c651e8fdf140382ae8e1b252954455838e4e12
Indro <indro.malleus@khk.ee>
2015-11-04 08:49:51 +0200

    lisatud lehe laadimisaeg

--- include/database.php
+++ include/database.php
@@ -1,22 +1,7 @@
 <?php
-<<<<<<< HEAD
+
 
 
 //connect database
 //wtf
 require "../config.sample.php";
-
-?>
-=======
-/**
- * Created by PhpStorm.
- * User: Indro
- * Date: 31.10.2015
- * Time: 15:00
- */
-//connect database
-$db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
-mysqli_query($db, "SET NAMES 'utf8'");
-
-require "config.sample.php";
->>>>>>> origin/master
