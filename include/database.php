3b993e1dfc5ff98a05eadffda9b36c579812f85c
Indro <indro.malleus@khk.ee>
2015-11-04 08:54:25 +0200

    parandus

--- include/database.php
+++ include/database.php
@@ -1,7 +1,8 @@
 <?php
 
 
-
 //connect database
-//wtf
-require "config.sample.php";
+$db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
+mysqli_query($db, "SET NAMES 'utf8'");
+
+require("config.sample.php");
