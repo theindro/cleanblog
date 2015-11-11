8976afb29d8dcce40ba2b3c252837b301e1d3f25
Indro <indro.malleus@khk.ee>
2015-11-11 11:20:03 +0200

    parandus

--- doc/database.sql
+++ doc/database.sql
@@ -1,110 +1,90 @@
--- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.26, for Win32 (x86)
 --
--- Host: 127.0.0.1
--- Loomise aeg: Okt 07, 2015 kell 09:45 p.k.
--- Serveri versioon: 5.6.26
--- PHP versioon: 5.6.12
+-- Host: localhost    Database: cleanblog
+-- ------------------------------------------------------
+-- Server version	5.6.26
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Andmebaas: `cleanblog`
---
-
--- --------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
+/*!40103 SET TIME_ZONE = '+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
+/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
 
 --
--- Tabeli struktuur tabelile `authors`
+-- Table structure for table `authors`
 --
 
-CREATE TABLE IF NOT EXISTS `authors` (
-  `author_id` int(11) unsigned NOT NULL,
-  `author_name` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors` (
+  `author_id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `author_name` VARCHAR(255)     NOT NULL,
+  PRIMARY KEY (`author_id`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Andmete tõmmistamine tabelile `authors`
+-- Dumping data for table `authors`
 --
 
-INSERT INTO `authors` (`author_id`, `author_name`) VALUES
-(1, 'sipsik'),
-(2, 'supsik'),
-(3, 'asdman');
-
--- --------------------------------------------------------
-
---
--- Tabeli struktuur tabelile `posts`
---
-
-CREATE TABLE IF NOT EXISTS `posts` (
-  `post_id` int(11) unsigned NOT NULL,
-  `author_id` int(10) unsigned NOT NULL,
-  `post_title` varchar(255) NOT NULL,
-  `post_description` varchar(255) NOT NULL,
-  `post_text` text NOT NULL,
-  `post_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+INSERT INTO `authors` VALUES (1, 'Klaabu'), (2, 'Sipsik');
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Andmete tõmmistamine tabelile `posts`
+-- Table structure for table `posts`
 --
 
-INSERT INTO `posts` (`post_id`, `author_id`, `post_title`, `post_description`, `post_text`, `post_created`) VALUES
-(1, 1, 'tiitel leht', 'jah jah jah', 'jejehejeh', '2015-10-07 22:36:33'),
-(3, 2, 'jaha ja', 'asd asd asd', 'wqweqwe', '2015-10-07 22:37:03'),
-(5, 3, 'Tere tulemast', 'description', 'text ja sisu', '2015-10-07 22:44:08');
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `post_id`          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `author_id`        INT(10) UNSIGNED NOT NULL,
+  `post_title`       VARCHAR(255)     NOT NULL,
+  `post_description` VARCHAR(255)              DEFAULT NULL,
+  `post_created`     DATETIME         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_text`        TEXT             NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`author_id`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 5
+  DEFAULT CHARSET = latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indeksid tõmmistatud tabelitele
+-- Dumping data for table `posts`
 --
 
---
--- Indeksid tabelile `authors`
---
-ALTER TABLE `authors`
-  ADD PRIMARY KEY (`author_id`);
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1, 1, 'asd', 'asd', '2015-10-08 10:37:36', 'asdasd\''),
+  (2, 2, 'boom    ', 'gwgsi   ', '2015-10-08 10:37:50', 'mison'),
+  (3, 1, 'jsinje      ', 'gpowjh  ', '2015-10-08 10:38:30', 'gifrhj'),
+  (4, 2, 'nojsa   ', 'bpfdjn', '2015-10-09 10:50:30', 'jgure');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
 
---
--- Indeksid tabelile `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`post_id`),
-  ADD KEY `author_id` (`author_id`);
+/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
 
---
--- AUTO_INCREMENT tõmmistatud tabelitele
---
-
---
--- AUTO_INCREMENT tabelile `authors`
---
-ALTER TABLE `authors`
-  MODIFY `author_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT tabelile `posts`
---
-ALTER TABLE `posts`
-  MODIFY `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
---
--- Tõmmistatud tabelite piirangud
---
-
---
--- Piirangud tabelile `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`author_id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Dump completed on 2015-11-05 10:36:40
