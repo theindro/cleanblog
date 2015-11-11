d49a39919f4aa778976c3987a7c4ec8bd626c824
Indro <indro.malleus@khk.ee>
2015-11-11 11:22:04 +0200

    parandus

--- include/database.php
+++ include/database.php
@@ -1,8 +1,5 @@
 <?php
-
-
-//connect database
+require 'config.php';
+// Connect database
 $db = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
-
-require("config.php");
